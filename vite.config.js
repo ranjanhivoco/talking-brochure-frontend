6e483a3e0f510e61bbf631a0204cce1087d58715
ranjanhivoco <ranjan@hivoco.com>
2025-08-01 10:31:26 +0530

    conifg

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss()],
   server: {
-    host: true,
-    port:8832,
+    host: "0.0.0.0",
+    port: 8834,
+    preview: {
+      host: "0.0.0.0",
+      port: 8834,
+      allowedHosts: [
+        "nestle.thefirstimpression.ai",
+        "localhost",
+        "127.0.0.1",
+      ],
+    },
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 8834,
+    allowedHosts: ["nestle.thefirstimpression.ai", "localhost", "127.0.0.1"],
   },
 });
