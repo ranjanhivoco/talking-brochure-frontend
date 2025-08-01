c756c2dd2960384e21855ad73b66e51987990e5c
ranjanhivoco <ranjan@hivoco.com>
2025-08-01 10:53:56 +0530

    conifg

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React  from "react"
+
 import Home from "./pages/Home";
 // import Test from "./components/Test";
 
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,14 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: true,
     port: 8834,
-    preview: {
-      host: "0.0.0.0",
-      port: 8834,
-      allowedHosts: [
-        "nestle.thefirstimpression.ai",
-        "localhost",
-        "127.0.0.1",
-      ],
-    },
   },
+
   preview: {
-    host: "0.0.0.0",
     port: 8834,
-    allowedHosts: ["nestle.thefirstimpression.ai", "localhost", "127.0.0.1"],
   },
 });
